92baeccbfa9064bf88472849010d0325f24bf892
Nedina Muratovic <nmuratovic3@etf.unsa.ba>
2020-11-06 19:14:56 +0100

    testSuperM

--- test/ba/unsa/etf/rpr/SupermarketTest.java
+++ test/ba/unsa/etf/rpr/SupermarketTest.java
@@ -19,5 +19,10 @@ class SupermarketTest {
 
     @Test
     void izbaciArtiklSaKodom() {
+        Supermarket market= new Supermarket();
+        Artikl cips= new Artikl("cips", 1, "254");
+        market.dodajArtikl(cips);
+        market.izbaciArtiklSaKodom("254");
+        assertEquals(1, market.getBroj());
     }
 }
